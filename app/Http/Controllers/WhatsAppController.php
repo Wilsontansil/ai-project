d7e862721b61031118deab7057d79c6be4265983
wilsontansil <tanwilson2410@gmail.com>
2026-04-09 10:27:21 +0700

    fix

--- app/Http/Controllers/WhatsAppController.php
+++ app/Http/Controllers/WhatsAppController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class WhatsAppController extends Controller
 {
     public function handleWebhook(Request $request)
     {
+        Log::info('Received WhatsApp webhook', ['payload' => $request->all()]);
         // Handle WhatsApp webhook here
         return response()->json(['status' => 'ok']);
     }

440bb2d69ee6fa57d827613b0282f3efd6126b13
wilsontansil <tanwilson2410@gmail.com>
2026-04-10 16:05:01 +0700

    fix

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,6 @@ use App\Http\Controllers\TelegramController;
 use App\Http\Controllers\WhatsAppController;
 use App\Http\Controllers\LiveChatController;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
 Route::get('/test', function () {
     return response()->json(['message' => 'API is working']);
 });

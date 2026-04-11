73702e283f355187e624262130e13befa49b3bbc
wilsontansil <tanwilson2410@gmail.com>
2026-04-12 02:07:17 +0700

    fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->redirectGuestsTo('/backoffice/login');
+        $middleware->redirectUsersTo('/backoffice');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //

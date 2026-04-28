512ccd3b261862daf481cbc0ce0e4ec16bc00c1e
wilsontansil <tanwilson2410@gmail.com>
2026-04-28 19:16:24 +0700

    fix

--- database/seeders/ToolSeeder.php
+++ database/seeders/ToolSeeder.php
@@ -75,7 +75,7 @@ class ToolSeeder extends Seeder
                     ],
                 ],
                 // 'keywords' => ['Reset Password', 'reset pass'],
-                'tool_rules' => "- Terima data user dalam format chat bebas\n- Jika sebagian data sudah ada, cukup minta data yang masih kurang\n- Jangan eksekusi sebelum semua data wajib terkumpul lengkap\n- Setelah berhasil, infokan password direset ke 1234567 dan berikan link ke website untuk login\n- Jika data tidak cocok, minta user periksa kembali atau tawarkan untuk dibantu REGISTER",
+                'tool_rules' => "- Terima data user dalam format chat bebas\n- Format data yang dibutuhkan: namarek, norek, bank\n- Jika sebagian data sudah ada, cukup minta data yang masih kurang\n- Jangan eksekusi sebelum semua data wajib terkumpul lengkap\n- Setelah berhasil, infokan bahwa password berhasil direset ke 1234567 dan berikan link website untuk login\n- Jika data tidak cocok, minta player periksa kembali atau tawarkan untuk dibantu REGISTER",
                 'information_text' => null,
                 'meta' => null,
             ],
